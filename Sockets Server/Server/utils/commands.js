c95d2720e765cf3499ecbe6012115b6306bfffcb
Marquezin <96223775+Marquezin@users.noreply.github.com>
2023-05-24 22:23:03 -0300

    Delete Sockets Server directory

--- Sockets Server/Server/utils/commands.js
+++
@@ -1,9 +0,0 @@
-function command(cmd, socket){
-    switch(cmd){ // Filtra oque o cliente mandou e procura se tem algum comando
-        case "inicio": 
-            socket.write("Iniciado!");
-        break;
-        // Continue seus comandos aqui em baixo!
-    }
-}
-module.exports = {command}
